8324ea06e23798f47c8fb65cc81dcfa7dc5c7699
BrendanLPi4 <brendannlu5@gmail.com>
2025-06-12 00:18:36 +1000

    MVP server still working: COMMIT skeleton repo setup

--- server/app.py
+++ server/app.py
@@ -11,28 +11,32 @@ USB_DEVICE_NAME = "E657-3701"
 videos_path = f"/media/brend/{USB_DEVICE_NAME}"
 
 def cleanup():
-    print("Running app.py `cleanup()`...")
+    print("\nRunning app.py `cleanup()`...")
 
 atexit.register(cleanup)
 
 def signal_handler(sig, frame):
     sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)   # ctrl+C
+signal.signal(signal.SIGTERM, signal_handler)  # kill or system shutdown
+signal.signal(signal.SIGQUIT, signal_handler)  # quit signal
 
 @app.route("/")
 def index(): 
     try:
         mp4_files = [f for f in os.listdir(videos_path) if f.endswith(".mp4")]
+        mp4_files.sort()
     except:
-        sys.exit("ERROR: issue accessing videos directory")
+        abort(404, descripton="Error displaying sorted .mp4 files")
+
     return render_template("index.html", files=mp4_files)
 
 @app.route("/video/<filename>")
 def serve_video(filename):
     safe_path = safe_join(videos_path, filename)
     if not safe_path or not os.path.isfile(safe_path):
-        abort(404)
+        abort(404, description="Error fetching files from specified drive in Flask app.py")
     return send_file(safe_path, mimetype="video/mp4")
 
 if __name__ == "__main__": 
