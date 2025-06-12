39124e698d124b32e22222d110b85031a8737524
BrendanLPi4 <brendannlu5@gmail.com>
2025-06-12 13:22:36 +1000

    initial diskmanagement scripts; bit janky not fully tested

---
+++ diskmanage/cleanlogs.py
@@ -0,0 +1,20 @@
+import os
+import sys
+sys.path.append(r"/home/brend/Documents")
+import timestamping
+
+LOGS_DIR_PATH = "/home/brend/Documents/diskmanage/logs"
+
+if __name__ == "__main__":
+    assert os.path.exists(LOGS_DIR_PATH) and os.path.isdir(LOGS_DIR_PATH)
+    to_clean = list(os.listdir(LOGS_DIR_PATH))
+    for f in to_clean:
+        assert f.endswith(".log"), f"NON LOG FILE FOUND: {f}"
+        assert timestamping.parse_filename(f, ".log"), f"MALFORMED LOG FILE FOUND: {f}"
+        fpath = os.path.join(LOGS_DIR_PATH, f)
+        assert os.path.isfile(fpath)
+        try:
+            os.remove(fpath)
+        except:
+            RuntimeError(f"COULD NOT DELETE {f}")
+    print(f"SUCCESSFULLY CLEANED {len(to_clean)} LOG FILES")
--- diskmanage/diskclean.py
+++ diskmanage/diskclean.py
@@ -1,5 +1,6 @@
 """Run with care this will screw with a dir if wrong
 """
+import logging
 
 from pathlib import Path
 import os
@@ -20,6 +21,7 @@ THRESHOLD = 0.8
 USB_DEVICE_NAME = "E657-3701"
 USB_PATH = os.path.join("/media/brend", USB_DEVICE_NAME)
 USB_VID_PATH = os.path.join(USB_PATH, "vidfiles")
+LOGS_DIR_PATH = "/home/brend/Documents/diskmanage/logs"
 
 def get_usb_usage(path: str, units_input: str = None):
     """Wrapper around shutil.disk_usage that returns:
@@ -54,16 +56,23 @@ def auto_cleanup(
     assert os.path.exists(clean_path) and os.path.isdir(clean_path)
     assert isinstance(threshold_ratio, float) and 0.5 < threshold_ratio < 1
     assert_subpath(child_path=clean_path, parent_path=monitor_path)
+    logging.info(f"`auto_cleanup()`: valid function call, monitor_path-> {monitor_path}, clean_path-> {clean_path}, threshold-> {threshold_ratio}")
 
     total_bytes, used_bytes, _ = get_usb_usage(path=monitor_path)
+    logging.info(f"`auto_cleanup()`: initial scan, currently using {used_bytes/GB}GB of total {total_bytes/GB}GB")
+
     to_clean_files = []
-    if used_bytes / total_bytes > 0: # TODO: CURRENTLY FOR TESTING
+    if used_bytes / total_bytes > threshold_ratio:
         to_clean_files = [
             f for f in os.listdir(clean_path) 
             if f.endswith(".mp4") and timestamping.parse_filename(f)
         ]
         to_clean_files.sort()
-    
+        logging.info(f"`auto_cleanup()`: initialized cleaning list with {len(to_clean_files)} cleanable video files")
+    else:
+        logging.info(f"`auto_cleanup()`: disk usage is {used_bytes*100/total_bytes}% for now, no cleaning to happen")
+        return 0
+
     cleaned_bytes = 0
     for file in to_clean_files:
         try:
@@ -71,18 +80,36 @@ def auto_cleanup(
             file_size = os.path.getsize(full_fpath)
             os.remove(full_fpath)
             cleaned_bytes += file_size
-            print(f"SUCCESS removing {file}")
+            logging.info(f"`auto_cleanup()`: {file} of {file_size} was removed, total cleaned bytes: {cleaned_bytes}")
         except:
-            print(f"ERROR removing {file}")
+            logging.error(f"`auto_cleanup()`: {file} was unable to be removed")
 
         if (used_bytes - cleaned_bytes) / total_bytes < threshold_ratio:
             break
     
     if (used_bytes - cleaned_bytes) / total_bytes > threshold_ratio:
-        print(f"MAJOR ERROR after cleaning all video files, disk usage still exceeds threshold")
+        logging.error(f"`auto_cleanup()`: after cleaning all cleanable video files, disk usage still exceeds threshold input: {threshold_ratio}")
 
     return cleaned_bytes
 
 if __name__ == "__main__":
-    print(get_usb_usage(USB_PATH, "gb"))
-    print(auto_cleanup(monitor_path=USB_PATH, clean_path=USB_VID_PATH, threshold_ratio=THRESHOLD))
+    # configure logger and generate timestamped log file name
+    timestamped_log_fname = timestamping.generate_filename(
+        camera_name=USB_DEVICE_NAME+"_CLEANLOG", # bit misleading naming but oh well...
+        extension=".log"
+    )
+    assert os.path.exists(LOGS_DIR_PATH) and os.path.isdir(LOGS_DIR_PATH)
+    logging.basicConfig(
+        level=logging.INFO,  # Decreasing verbosity: DEBUG, INFO, WARNING, ERROR, CRITICAL
+        format='%(asctime)s [%(levelname)s] %(message)s',
+        handlers=[
+            logging.FileHandler(os.path.join(LOGS_DIR_PATH, timestamped_log_fname)),
+            logging.StreamHandler()  # also prints to console
+        ]
+    )
+
+    try:
+        # run some sort of loop here
+        auto_cleanup(monitor_path=USB_PATH, clean_path=USB_VID_PATH, threshold_ratio=THRESHOLD)
+    except:
+        logging.critical("main: `auto_cleanup()` call caused exception")
